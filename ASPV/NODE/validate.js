6cf0c449ce90ef4c401989669112a182851e46e4
Hanul <hanul@hanul.me>
2016-07-07 19:02:54 +0900

    자체 개발

--- ASPV/NODE/validate.js
+++ ASPV/NODE/validate.js
@@ -1,56 +1,65 @@
-ASPV.validate = METHOD(function(m) {
+ASPV.validate = METHOD({
 	
-	var
-	// api verifier
-	apiVerifier = require('iap_verifier'),
-	
-	// client
-	client;
-	
-	return {
+	run : function(params, callback) {
+		//REQUIRED: params.productId
+		//REQUIRED: params.receipt
+		//REQUIRED: callback
 		
-		run : function(params, callback) {
-			//REQUIRED: params.productId
-			//REQUIRED: params.receipt
-			//REQUIRED: callback
+		var
+		// product id
+		productId = params.productId,
+		
+		// receipt
+		receipt = params.receipt,
+		
+		// host
+		host = 'buy.itunes.apple.com';
+		
+		RUN(function(retry) {
 			
-			var
-			// product id
-			productId = params.productId,
-			
-			// receipt
-			receipt = params.receipt;
-			
-			if (client === undefined) {
-				client = new apiVerifier(NODE_CONFIG.ASPV.sharedSecretKey);
-			}
-			
-			client.verifyReceipt(receipt, true, function(isValid, notUsing, data) {
+			POST({
+				isSecure : true,
+				host : host,
+				uri : 'verifyReceipt',
+				paramStr : JSON.stringify({
+					'receipt-data' : receipt
+				})
+			}, function(content) {
 				
 				var
-				// return
-				ret = false;
+				// data
+				data = PARSE_STR(content),
 				
-				if (isValid === true && data.receipt !== undefined) {
+				// is valid
+				isValid = false;
+				
+				if (data.status === 0 && data.receipt !== undefined) {
 					
 					// iOS <= 6
 					if (data.receipt.product_id === productId) {
-						ret = true;
+						isValid = true;
 					}
 					
 					// iOS >= 7
 					else if (data.receipt.in_app !== undefined) {
 						EACH(data.receipt.in_app, function(iapInfo) {
 							if (iapInfo.product_id === productId) {
-								ret = true;
+								isValid = true;
 								return false;
 							}
 						});
 					}
 				}
 				
-				callback(ret);
+				if (isValid !== true && host !== 'sandbox.itunes.apple.com') {
+					host = 'sandbox.itunes.apple.com';
+					retry();
+				}
+				
+				else {
+					callback(isValid);
+				}
 			});
-		}
-	};
+		});
+	}
 });
--- README.md
+++ README.md
@@ -1,29 +1,12 @@
 # AppStorePurchaseValidator
 앱스토어 결제 검증 BOX
 
-## 설정
-```javascript
-require(process.env.UPPERCASE_PATH + '/BOOT.js');
-
-BOOT({
-	CONFIG : {
-		...
-	},
-
-	NODE_CONFIG : {
-	
-		ASPV : {
-			sharedSecretKey : '~~~'
-		},
-		
-		...
-	}
-});
-```
-
 ## 사용방법
 ```javascript
-ASPV.validate('{{receipt}}', function(isValid) {
+ASPV.validate({
+	productId : '{{productId}}',
+	receipt : '{{receipt}}'
+}, function(isValid) {
 	console.log(isValid);
 });
 ```
