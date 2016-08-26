f2584c746a4ad73f89b8a03efac0b99f3953caa1
Hanul <hanul@hanul.me>
2016-08-26 18:01:41 +0900

    add error handler

--- ASPV/NODE/validate.js
+++ ASPV/NODE/validate.js
@@ -1,9 +1,11 @@
 ASPV.validate = METHOD({
 	
-	run : function(params, callback) {
+	run : function(params, callbackOrHandlers) {
 		//REQUIRED: params.productId
 		//REQUIRED: params.receipt
-		//REQUIRED: callback
+		//REQUIRED: callbackOrHandlers
+		//OPTIONAL: callbackOrHandlers.error
+		//OPTIONAL: callbackOrHandlers.success
 		
 		var
 		// product id
@@ -11,53 +13,95 @@ ASPV.validate = METHOD({
 		
 		// receipt
 		receipt = params.receipt,
+			
+		// error handler.
+		errorHandler,
+		
+		// callback.
+		callback,
 		
 		// host
 		host = 'buy.itunes.apple.com';
 		
+		if (callbackOrHandlers !== undefined) {
+			
+			if (CHECK_IS_DATA(callbackOrHandlers) !== true) {
+				callback = callbackOrHandlers;
+			} else {
+				errorHandler = callbackOrHandlers.error;
+				callback = callbackOrHandlers.success;
+			}
+		}
+		
 		RUN(function(retry) {
 			
-			POST({
+			var
+			// params
+			params;
+			
+			POST(params = {
 				isSecure : true,
 				host : host,
 				uri : 'verifyReceipt',
 				paramStr : JSON.stringify({
 					'receipt-data' : receipt
 				})
-			}, function(content) {
-				
-				var
-				// data
-				data = PARSE_STR(content),
-				
-				// is valid
-				isValid = false;
-				
-				if (data.status === 0 && data.receipt !== undefined) {
+			}, {
+				error : function(errorMsg) {
 					
-					// iOS <= 6
-					if (data.receipt.product_id === productId) {
-						isValid = true;
+					if (errorHandler !== undefined) {
+						errorHandler();
+					} else {
+						SHOW_ERROR('[ASPV] Error!', errorMsg, params);
+					}
+				},
+				
+				success : function(content) {
+					
+					var
+					// data
+					data = PARSE_STR(content),
+					
+					// is valid
+					isValid = false;
+					
+					if (data === undefined) {
+						if (errorHandler !== undefined) {
+							errorHandler();
+						} else {
+							SHOW_ERROR('[ASPV] Error! Data:', content);
+						}
 					}
 					
-					// iOS >= 7
-					else if (data.receipt.in_app !== undefined) {
-						EACH(data.receipt.in_app, function(iapInfo) {
-							if (iapInfo.product_id === productId) {
+					else {
+						
+						if (data.status === 0 && data.receipt !== undefined) {
+							
+							// iOS <= 6
+							if (data.receipt.product_id === productId) {
 								isValid = true;
-								return false;
 							}
-						});
+							
+							// iOS >= 7
+							else if (data.receipt.in_app !== undefined) {
+								EACH(data.receipt.in_app, function(iapInfo) {
+									if (iapInfo.product_id === productId) {
+										isValid = true;
+										return false;
+									}
+								});
+							}
+						}
+						
+						if (isValid !== true && host !== 'sandbox.itunes.apple.com') {
+							host = 'sandbox.itunes.apple.com';
+							retry();
+						}
+						
+						else if (callback !== undefined) {
+							callback(isValid);
+						}
 					}
-				}
-				
-				if (isValid !== true && host !== 'sandbox.itunes.apple.com') {
-					host = 'sandbox.itunes.apple.com';
-					retry();
-				}
-				
-				else {
-					callback(isValid);
 				}
 			});
 		});
